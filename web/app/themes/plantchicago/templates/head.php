20e71f0f896d6d219987b1f75fd6230e9b59b82d
Matt Soria <matt.m.soria@gmail.com>
2015-12-21 15:11:55 -0600

    Adding favicon

--- web/app/themes/plantchicago/templates/head.php
+++ web/app/themes/plantchicago/templates/head.php
@@ -2,6 +2,8 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon-32x32.png') ?>" />
+  <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
   <link href='https://fonts.googleapis.com/css?family=Karla:400,400italic,700' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
   <meta name="description" content="">
