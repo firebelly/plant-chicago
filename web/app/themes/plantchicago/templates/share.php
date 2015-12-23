8dd74d9b1d5d74dcd6dfb607018eaee30bdd332e
Matt Soria <matt.m.soria@gmail.com>
2015-12-23 11:57:03 -0600

    Opening share links in new tab

--- web/app/themes/plantchicago/templates/share.php
+++ web/app/themes/plantchicago/templates/share.php
@@ -2,10 +2,10 @@
   <h4 class="sr-only">Share on social media:</h4>
   <ul class="social">
     <li>
-      <a href="https://www.facebook.com/sharer/sharer.php?u=<?= get_permalink( $post->ID ); ?>"><span class="sr-only">Facebook</span><svg class="icon icon-facebook"><use xlink:href="#icon-facebook"></svg></a>
+      <a href="https://www.facebook.com/sharer/sharer.php?u=<?= get_permalink( $post->ID ); ?>" target="_blank"><span class="sr-only">Facebook</span><svg class="icon icon-facebook"><use xlink:href="#icon-facebook"></svg></a>
     </li>
     <li>
-      <a href="https://twitter.com/share?text=<?= the_title(); ?>&via=plantchicago&url=<?= get_permalink( $post->ID ) ?>"><span class="sr-only">Twitter</span><svg class="icon icon-twitter"><use xlink:href="#icon-twitter"></svg></a>
+      <a href="https://twitter.com/share?text=<?= the_title(); ?>&via=plantchicago&url=<?= get_permalink( $post->ID ) ?>" target="_blank"><span class="sr-only">Twitter</span><svg class="icon icon-twitter"><use xlink:href="#icon-twitter"></svg></a>
     </li>
   </ul>
 </div>
