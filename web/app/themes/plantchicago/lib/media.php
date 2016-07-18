5e740af95d7bcf12d1a2ee313f878df2773ead61
Nate Beaty <nate@firebellydesign.com>
2016-07-18 12:55:57 -0500

    add function to get absolute path to sized thumbnail instead of URL

--- web/app/themes/plantchicago/lib/media.php
+++ web/app/themes/plantchicago/lib/media.php
@@ -23,7 +23,34 @@ function get_post_thumbnail($post_id, $size='medium') {
 }
 
 /**
- * Get header bg for post, duotone treated with the random IHC_BACKGROUND + Dark Blue  
+ * Get the file path (not URL) to a thumbnail of a particular size.
+ * (get_attached_file() only returns paths to full-sized thumbnails.)
+ * @param  int            $thumb_id - attachment id of thumbnail
+ * @param  string|array   $size - thumbnail size string (e.g. 'full') or array [w,h]
+ * @return path           file path to properly sized thumbnail
+ */
+function get_thumbnail_size_path($thumb_id, $size) {
+  // Find the path to the root image. We can get this from get_attached_file.
+  $old_path = get_attached_file($thumb_id, true);
+
+  // Find the url of the image with the proper size
+  $attr = wp_get_attachment_image_src($thumb_id, $size);
+  $url = $attr[0];
+
+  // Grab the filename of the sized image from the url
+  $exploded_url = explode('/', $url);
+  $filename = $exploded_url[count($exploded_url)-1];
+
+  // Replace the filename in our path with the filename of the properly sized image
+  $exploded_path = explode('/', $old_path);
+  $exploded_path[count($exploded_path)-1] = $filename;
+  $new_path = implode ('/', $exploded_path);
+
+  return $new_path;
+}
+
+/**
+ * Get header bg for post, duotone treated with the random IHC_BACKGROUND + Dark Blue
  * @param  string|object   $post_or_image (WP post object or background image)
  * @return HTML            background image code
  */
@@ -39,7 +66,7 @@ function get_header_bg($post_or_image, $thumb_id='', $options=[]) {
   if (is_object($post_or_image)) {
     if (has_post_thumbnail($post_or_image->ID)) {
       $thumb_id = get_post_thumbnail_id($post_or_image->ID);
-      $background_image = wp_get_attachment_image_src($thumb_id, $size)[0];
+      $background_image = get_thumbnail_size_path($thumb_id, $size);
     }
   } else {
     // These are sent from a taxonomy page
@@ -52,7 +79,7 @@ function get_header_bg($post_or_image, $thumb_id='', $options=[]) {
     // Build treated filename with thumb_id in case there are filename conflicts
     $treated_filename = preg_replace("/.+\/(.+)\.(\w{2,5})$/", $thumb_id."-$1-".$color.".$2", $background_image);
     $treated_image = $base_dir . $treated_filename;
-  
+
     // If treated file doesn't exist, create it
     if (!file_exists($treated_image)) {
 
@@ -62,7 +89,7 @@ function get_header_bg($post_or_image, $thumb_id='', $options=[]) {
       }
       $convert_command = (WP_ENV==='development') ? '/usr/local/bin/convert' : '/usr/bin/convert';
       exec($convert_command.' '.$background_image.' +profile "*" -quality 65 -modulate 100,0 -size 256x1! gradient:#414141-#'.$color.' -clut '.$treated_image);
-    }    
+    }
     $header_bg = ' style="background-image:url(' . $upload_dir['baseurl'] . '/backgrounds/' . $treated_filename . ');"';
   }
   return $header_bg;
