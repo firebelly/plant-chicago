794f07ffc41036cdd4e86fc2f366e57058e77874
poopsplat <matt.m.soria@gmail.com>
2019-06-06 13:21:20 -0500

    Adding google tag manager scripts to head and body elements per client request

--- web/app/themes/plantchicago/base.php
+++ web/app/themes/plantchicago/base.php
@@ -12,6 +12,11 @@ use Roots\Sage\Wrapper;
 <!--[if gt IE 9]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MHPK634"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <!--[if lt IE 9]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
--- web/app/themes/plantchicago/templates/head.php
+++ web/app/themes/plantchicago/templates/head.php
@@ -12,4 +12,11 @@
   <?php if ($fb_image) : ?>
     <meta property="og:image" content="<?php echo $fb_image[0]; ?>" />
   <?php endif; ?>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-MHPK634');</script>
+  <!-- End Google Tag Manager -->
 </head>
