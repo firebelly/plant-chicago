3e4562f125dd5d16f61881ae70406a7a2479deb9
poopsplat <matt.m.soria@gmail.com>
2019-06-07 09:31:40 -0500

    Moving google tag manager code up in the head a bit

--- web/app/themes/plantchicago/templates/head.php
+++ web/app/themes/plantchicago/templates/head.php
@@ -4,6 +4,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon-32x32.png') ?>" />
   <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-MHPK634');</script>
+  <!-- End Google Tag Manager -->
   <link href='https://fonts.googleapis.com/css?family=Karla:400,400italic,700' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
   <meta name="description" content="">
@@ -12,11 +19,4 @@
   <?php if ($fb_image) : ?>
     <meta property="og:image" content="<?php echo $fb_image[0]; ?>" />
   <?php endif; ?>
-  <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-MHPK634');</script>
-  <!-- End Google Tag Manager -->
 </head>
