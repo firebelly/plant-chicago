69bb64b1e13114434affbdf0fabd161f2a148ec0
poopsplat <matt.m.soria@gmail.com>
2019-06-11 11:25:44 -0500

    Removing soil-google-analytics and moving the GTM code up in the head

--- web/app/themes/plantchicago/templates/head.php
+++ web/app/themes/plantchicago/templates/head.php
@@ -1,9 +1,4 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon-32x32.png') ?>" />
-  <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
   <!-- Google Tag Manager -->
   <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -11,6 +6,11 @@
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','GTM-MHPK634');</script>
   <!-- End Google Tag Manager -->
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon-32x32.png') ?>" />
+  <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
   <link href='https://fonts.googleapis.com/css?family=Karla:400,400italic,700' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
   <meta name="description" content="">
