b1473accb33a6ad869f48773ba271d8c32241770
poopsplat <matt.m.soria@gmail.com>
2019-11-18 10:26:56 -0600

    Temporarily removing the addition newsletter feild from the form in the footer

--- web/app/themes/plantchicago/templates/newsletter.php
+++ web/app/themes/plantchicago/templates/newsletter.php
@@ -1,9 +1,5 @@
 <form class="newsletter-form" action="http://plantchicago.createsend.com/t/r/s/ajdjky/" method="post">
   <div class="row">
-    <label for="fieldName" class="sr-only">Full Name</label>
-    <input type="text" aria-label="Name" id="fieldName" maxlength="200" name="cm-name" placeholder="Your full name" required>
-  </div>
-  <div class="row">
     <label for="fieldEmail" class="sr-only">Email</label>
     <input id="fieldEmail" name="cm-ajdjky-ajdjky" type="email" placeholder="Your email address" required>
     <button type="submit">Go <span class="arrow"></span></button>
